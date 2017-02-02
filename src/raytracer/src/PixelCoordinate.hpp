1076b26bcfce9eddc523b2c6ca41a20d5400c6b9
Will Price <will.price94@gmail.com>
2017-02-02 15:49:02 +0000

    Try to project rays from camera centre into triangles and render result - buggy

---
+++ src/raytracer/src/PixelCoordinate.hpp
@@ -0,0 +1,12 @@
+
+#ifndef RAYTRACER_PIXELCOORDINATE_HPP
+#define RAYTRACER_PIXELCOORDINATE_HPP
+
+
+class PixelCoordinate {
+  const uint x;
+  const uint y;
+};
+
+
+#endif //RAYTRACER_PIXELCOORDINATE_HPP
--- src/raytracer/src/raytracer.cpp
+++ src/raytracer/src/raytracer.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <map>
 
 #include "interpolation.hpp"
 #include "projection.hpp"
@@ -14,9 +15,7 @@ using namespace std;
 using namespace cg;
 using glm::vec2;
 using glm::vec3;
-using glm::vec4;
 using glm::mat3;
-using glm::mat4;
 
 /* ----------------------------------------------------------------------------*/
 /* GLOBAL VARIABLES                                                            */
@@ -26,21 +25,17 @@ const int SCREEN_HEIGHT = 500;
 const float WORLD_WIDTH = 2;
 const float WORLD_HEIGHT = 2;
 const float WORLD_DEPTH = 2;
-const float star_velocity = -0.0001;
-const float FOCAL_LENGTH = 10;
+const float FOCAL_LENGTH = 1;
+
+vector<float> screen_pixel_centres_y(SCREEN_HEIGHT);
+vector<float> screen_pixel_centres_x(SCREEN_WIDTH);
+vector<Intersection> rayIntersections;
 
 SDL_Surface* screen;
 int t;
-vector<vec4> stars(1000);
 vector<Triangle> triangles;
 
-vec4 camera_centre(0, 0, 0, 1);
-mat4 world_to_camera = {
-        vec4(1, 0, 0, 0),
-        vec4(0, 1, 0, 0),
-        vec4(0, 0, 1, 0),
-        vec4(-camera_centre.x, -camera_centre.y, -camera_centre.z, 1)
-};
+vec3 camera_centre(0, 0, -2);
 
 
 /* ----------------------------------------------------------------------------*/
@@ -59,14 +54,20 @@ int main( int argc, char* argv[] )
   screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
   t = SDL_GetTicks();	// Set start value for timer.
 
-  for (size_t i = 0; i < stars.size(); i++) {
-    stars[i].x = rescale(-1, 1, randomProbability());
-    stars[i].y = rescale(-1, 1, randomProbability());
-    stars[i].z = randomProbability();
-    stars[i][3] = 1;
-  }
-
   LoadTestModel(triangles);
+
+  vec3 topLeft = vec3((-WORLD_WIDTH / 2) + (WORLD_WIDTH / (2 * SCREEN_WIDTH)),
+                      (-WORLD_HEIGHT / 2) + (WORLD_HEIGHT / (2 * SCREEN_HEIGHT)),
+                      FOCAL_LENGTH);
+
+  vec3 bottomRight = vec3((WORLD_WIDTH / 2) - (WORLD_WIDTH / (2 * SCREEN_WIDTH)),
+                          (WORLD_HEIGHT / 2) - (WORLD_HEIGHT / (2 * SCREEN_HEIGHT)),
+                          FOCAL_LENGTH);
+
+  vec3 bottomLeft = vec3(topLeft.x, bottomRight.y, FOCAL_LENGTH);
+  vec3 topRight = vec3(bottomRight.x, topLeft.y, FOCAL_LENGTH);
+  interpolate(topLeft.y, bottomLeft.y, screen_pixel_centres_y);
+  interpolate(topLeft.x, topRight.x, screen_pixel_centres_x);
 
   while( NoQuitMessageSDL() )
   {
@@ -76,7 +77,40 @@ int main( int argc, char* argv[] )
 
   return 0;
 }
+bool closest_intersection(vec3 start, vec3 direction, const vector<Triangle>& triangles, Intersection& closestIntersection) {
+  vector<Intersection> validIntersection;
+  for (int i = 0; i < triangles.size(); i++) {
+    Triangle triangle = triangles[i];
+    mat3 A(-direction, triangle.e1(), triangle.e2());
+    vec3 b = start - triangle.v0;
+    if (glm::determinant(A) == 0) {
+      continue;
+    }
+    vec3 intersection_point = glm::inverse(A)*b;
+    float u = intersection_point.y;
+    float v = intersection_point.z;
+    float t = intersection_point.x;
+    if (u > 0 && v > 0 && u + v < 1 && t >= 0) {
+      Intersection intersection;
+      intersection.distance = glm::length(t*direction);
+      intersection.position = start + t*direction;
+      intersection.triangleIndex = i;
+      validIntersection.push_back(intersection);
+    }
+  }
 
+  if (validIntersection.size() == 0) {
+    return false;
+  }
+
+  closestIntersection = validIntersection.front();
+  for (Intersection intersection : validIntersection) {
+    if (intersection.distance < closestIntersection.distance) {
+      closestIntersection = intersection;
+    }
+  }
+  return true;
+}
 void Update()
 {
   // Compute frame time:
@@ -85,38 +119,16 @@ void Update()
   t = t2;
   cout << "Render time: " << dt << " ms." << endl;
 
-  vec3 topLeft = vec3((-WORLD_WIDTH / 2) + (WORLD_WIDTH / (2 * SCREEN_WIDTH)),
-                      (-WORLD_HEIGHT / 2) + (WORLD_HEIGHT / (2 * SCREEN_HEIGHT)),
-                      FOCAL_LENGTH);
-
-  vec3 bottomRight = vec3((WORLD_WIDTH / 2) - (WORLD_WIDTH / (2 * SCREEN_WIDTH)),
-                      (WORLD_HEIGHT / 2) - (WORLD_HEIGHT / (2 * SCREEN_HEIGHT)),
-                      FOCAL_LENGTH);
-
-  vec3 bottomLeft = vec3(topLeft.x, bottomRight.y, FOCAL_LENGTH);
-  vec3 topRight = vec3(bottomRight.x, topLeft.y, FOCAL_LENGTH);
-  vector<float> screen_pixel_centres_y(SCREEN_HEIGHT);
-  interpolate(topLeft.y, bottomLeft.y, screen_pixel_centres_y);
-  vector<float> screen_pixel_centres_x(SCREEN_WIDTH);
-  interpolate(topLeft.x, topRight.x, screen_pixel_centres_x);
 
 
   for (int y = 0; y < screen_pixel_centres_y.size(); y++) {
     for (int x = 0; x < screen_pixel_centres_x.size(); x++) {
       vec3 pixel_centre(screen_pixel_centres_x[x], screen_pixel_centres_y[y]
         , FOCAL_LENGTH);
-      for (Triangle t : triangles) {
-
+      Intersection closestIntersection;
+      if (closest_intersection(camera_centre, pixel_centre, triangles, closestIntersection)) {
+        rayIntersections.push_back(closestIntersection);
       }
-    }
-  }
-
-
-  for (size_t i = 0; i < stars.size(); i++) {
-    float *z = &stars[i].z;
-    *z += star_velocity*dt;
-    if (*z <= 0) {
-      *z = 1;
     }
   }
 
@@ -145,10 +157,6 @@ void Update()
   {
     camera_centre.z -= 0.0001;
   }
-
-  world_to_camera[3][0] = -camera_centre.x;
-  world_to_camera[3][1] = -camera_centre.y;
-  world_to_camera[3][2] = -camera_centre.z;
 }
 
 void Draw()
@@ -158,19 +166,13 @@ void Draw()
   if (SDL_MUSTLOCK(screen))
     SDL_LockSurface(screen);
 
-
-  for (size_t i = 0; i < stars.size(); i++) {
-    float z = stars[i].z;
-    // Star colour is proportional to the inverse square of distance (standard photon equation)
-    vec3 star_colour(1, 1, 1); // 0.2f * vec3(1, 1, 1) / (z * z);
-    vec4 &world_star = stars[i];
-    vec4 camera_star = world_to_camera*world_star;
-    vec2 projection = project(screen, camera_star, FOCAL_LENGTH);
-    PutPixelSDL(screen, projection.x, projection.y, star_colour);
-  }
-
   if (SDL_MUSTLOCK(screen))
     SDL_UnlockSurface(screen);
 
+  for (Intersection intersection : rayIntersections) {
+    Triangle triangle = triangles[intersection.triangleIndex];
+    vec2 point = project(screen, intersection.position, FOCAL_LENGTH);
+    PutPixelSDL(screen, point.x, point.y, triangle.color);
+  }
   SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
