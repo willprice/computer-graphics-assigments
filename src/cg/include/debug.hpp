8fd038038820e062c5c7e276a78c4ec6f3f7f918
Will Price <will.price94@gmail.com>
2017-03-01 12:52:31 +0000

    Make debug functions const

--- src/cg/include/debug.hpp
+++ src/cg/include/debug.hpp
@@ -4,8 +4,8 @@
 #include <glm/glm.hpp>
 
 namespace cg {
-std::ostream& operator<<(std::ostream& os, glm::vec3& triangle);
-std::ostream& operator<<(std::ostream& os, glm::ivec2 &v);
-std::ostream &operator<<(std::ostream &os, glm::vec2 &v);
+std::ostream& operator<<(std::ostream& os, const glm::vec3& triangle);
+std::ostream& operator<<(std::ostream& os, const glm::ivec2 &v);
+std::ostream &operator<<(std::ostream &os, const glm::vec2 &v);
 }
 #endif //RAYTRACER_DEBUG_HPP
--- src/cg/src/debug.cpp
+++ src/cg/src/debug.cpp
@@ -6,16 +6,16 @@ namespace cg {
 using glm::vec3;
 using glm::vec2;
 using glm::ivec2;
-std::ostream &operator<<(std::ostream &os, vec3 &v) {
+std::ostream &operator<<(std::ostream &os, const vec3 &v) {
   os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
   return os;
 }
-std::ostream &operator<<(std::ostream &os, vec2 &v) {
+std::ostream &operator<<(std::ostream &os, const vec2 &v) {
   os << "(" << v.x << ", " << v.y << ")";
   return os;
 }
 
-std::ostream &operator<<(std::ostream &os, ivec2 &v) {
+std::ostream &operator<<(std::ostream &os, const ivec2 &v) {
   os << "(" << v.x << ", " << v.y << ")";
   return os;
 }
