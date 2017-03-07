4b361bfb5bbb44b013ab19f669c0c013e538e92c
Will Price <will.price94@gmail.com>
2017-03-07 20:56:11 +0000

    Remove build_release folder

--- build_release/src/cg/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ms14981/Documents/y3/graphics/template/ray-tracer/src/cg/src
-# Build directory: /home/ms14981/Documents/y3/graphics/template/ray-tracer/build_release/src/cg/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_release/src/cg/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ms14981/Documents/y3/graphics/template/ray-tracer/src/cg/test
-# Build directory: /home/ms14981/Documents/y3/graphics/template/ray-tracer/build_release/src/cg/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CG_tests "/home/ms14981/Documents/y3/graphics/template/ray-tracer/build_release/bin/CG_tests")
