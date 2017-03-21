49eef660de596eb519f2a0c4c7d0f0282d489e45
Matthew Smith <ms14981.2014@my.bristol.ac.uk>
2017-03-21 15:33:56 +0000

    Changed colours of Cornell Box

--- src/cg/src/models/cornell_box.cpp
+++ src/cg/src/models/cornell_box.cpp
@@ -12,6 +12,8 @@ void LoadTestModel(std::vector<Triangle> &triangles) {
   vec3 blue(0.15f, 0.15f, 0.75f);
   vec3 purple(0.75f, 0.15f, 0.75f);
   vec3 white(0.75f, 0.75f, 0.75f);
+  vec3 persianRed(0.84f,0.16f,0.18f);
+  vec3 hippieGreen(0.36f,0.55f,0.25f);
 
   triangles.clear();
   triangles.reserve(5 * 2 * 3);
@@ -32,20 +34,20 @@ void LoadTestModel(std::vector<Triangle> &triangles) {
   vec3 H(0, L, L);
 
   // Floor:
-  triangles.push_back(Triangle(C, B, A, green));
-  triangles.push_back(Triangle(C, D, B, green));
+  triangles.push_back(Triangle(C, B, A, white));
+  triangles.push_back(Triangle(C, D, B, white));
 
   // Left wall
-  triangles.push_back(Triangle(A, E, C, purple));
-  triangles.push_back(Triangle(C, E, G, purple));
+  triangles.push_back(Triangle(A, E, C, persianRed));
+  triangles.push_back(Triangle(C, E, G, persianRed));
 
   // Right wall
-  triangles.push_back(Triangle(F, B, D, yellow));
-  triangles.push_back(Triangle(H, F, D, yellow));
+  triangles.push_back(Triangle(F, B, D, hippieGreen));
+  triangles.push_back(Triangle(H, F, D, hippieGreen));
 
   // Ceiling
-  triangles.push_back(Triangle(E, F, G, cyan));
-  triangles.push_back(Triangle(F, H, G, cyan));
+  triangles.push_back(Triangle(E, F, G, white));
+  triangles.push_back(Triangle(F, H, G, white));
 
   // Back wall
   triangles.push_back(Triangle(G, D, C, white));
@@ -65,24 +67,24 @@ void LoadTestModel(std::vector<Triangle> &triangles) {
   H = vec3(82, 165, 225);
 
   // Front
-  triangles.push_back(Triangle(E, B, A, red));
-  triangles.push_back(Triangle(E, F, B, red));
+  triangles.push_back(Triangle(E, B, A, white));
+  triangles.push_back(Triangle(E, F, B, white));
 
   // Front
-  triangles.push_back(Triangle(F, D, B, red));
-  triangles.push_back(Triangle(F, H, D, red));
+  triangles.push_back(Triangle(F, D, B, white));
+  triangles.push_back(Triangle(F, H, D, white));
 
   // BACK
-  triangles.push_back(Triangle(H, C, D, red));
-  triangles.push_back(Triangle(H, G, C, red));
+  triangles.push_back(Triangle(H, C, D, white));
+  triangles.push_back(Triangle(H, G, C, white));
 
   // LEFT
-  triangles.push_back(Triangle(G, E, C, red));
-  triangles.push_back(Triangle(E, A, C, red));
+  triangles.push_back(Triangle(G, E, C, white));
+  triangles.push_back(Triangle(E, A, C, white));
 
   // TOP
-  triangles.push_back(Triangle(G, F, E, red));
-  triangles.push_back(Triangle(G, H, F, red));
+  triangles.push_back(Triangle(G, F, E, white));
+  triangles.push_back(Triangle(G, H, F, white));
 
   // ---------------------------------------------------------------------------
   // Tall block
@@ -98,24 +100,24 @@ void LoadTestModel(std::vector<Triangle> &triangles) {
   H = vec3(314, 330, 456);
 
   // Front
-  triangles.push_back(Triangle(E, B, A, blue));
-  triangles.push_back(Triangle(E, F, B, blue));
+  triangles.push_back(Triangle(E, B, A, white));
+  triangles.push_back(Triangle(E, F, B, white));
 
   // Front
-  triangles.push_back(Triangle(F, D, B, blue));
-  triangles.push_back(Triangle(F, H, D, blue));
+  triangles.push_back(Triangle(F, D, B, white));
+  triangles.push_back(Triangle(F, H, D, white));
 
   // BACK
-  triangles.push_back(Triangle(H, C, D, blue));
-  triangles.push_back(Triangle(H, G, C, blue));
+  triangles.push_back(Triangle(H, C, D, white));
+  triangles.push_back(Triangle(H, G, C, white));
 
   // LEFT
-  triangles.push_back(Triangle(G, E, C, blue));
-  triangles.push_back(Triangle(E, A, C, blue));
+  triangles.push_back(Triangle(G, E, C, white));
+  triangles.push_back(Triangle(E, A, C, white));
 
   // TOP
-  triangles.push_back(Triangle(G, F, E, blue));
-  triangles.push_back(Triangle(G, H, F, blue));
+  triangles.push_back(Triangle(G, F, E, white));
+  triangles.push_back(Triangle(G, H, F, white));
 
   // ----------------------------------------------
   // Scale to the volume [-1,1]^3
